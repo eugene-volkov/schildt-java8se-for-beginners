9f49c4116e8c296782f952bbf8895396d145cae4
Eugene Volkov <bizzar@list.ru>
2019-09-30 17:06:41 +0300

    add src, upd gitignore

---
+++ src/ch1/Example.java
@@ -0,0 +1,9 @@
+package ch1;
+
+public class Example {
+    // выполнение любой программы на Java начинается
+    // с вызова метода main()
+    public static void main(String[] args) {
+        System.out.println("Привет, мир!");
+    }
+}
